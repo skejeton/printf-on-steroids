59ed93e9b36a51a2a24e2254e3cda4743e5d76f1
bumbread <thebumboni@gmail.com>
2022-08-17 22:09:16 +1100

    Use D3D11 on windows

--- server/SokolImpl.cpp
+++ server/SokolImpl.cpp
@@ -2,7 +2,13 @@
 // It's not changes often but sokol drags the compile times down quite a lot.
 
 #define SOKOL_IMPL
-#define SOKOL_GLCORE33
+
+#if _WIN32
+    #define SOKOL_D3D11
+#else
+    #define SOKOL_GLCORE33
+#endif
+
 #include "lib/sokol/sokol_app.h"
 #include "lib/sokol/sokol_gfx.h"
 #include "lib/sokol/sokol_glue.h"
